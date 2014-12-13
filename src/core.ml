2dc28398aa6a6f4eb2708f887c9dc0fa670fb05b
Roma Sokolov <sokolov.r.v@gmail.com>
2014-12-14 02:41:56 +0300

    [change] Manually passing to_js/from_js functions

--- src/core.ml
+++ src/core.ml
@@ -19,10 +19,11 @@ let to_obj l = Js.Unsafe.obj @@ Array.of_list l
 let jss s = Js.string s
 let inj o = Js.Unsafe.inject o
 
+
+
 module type REACT = sig
     type component
-    type value_t = unit
-    val component : (value_t -> component) -> (value_t -> component)
+    val component : ('a -> component) -> ('a -> 'b) -> ('c -> 'a) -> ('a -> component)
     val element_of_tag : string -> (string * Js.Unsafe.any) list -> string -> component
     val render : component -> Dom_html.element Js.t -> unit
   end
@@ -30,19 +31,20 @@ module type REACT = sig
 module React:REACT = struct
   let react = (Js.Unsafe.variable "React")
   type component = Js.Unsafe.any
-  type value_t = unit
 
-  let component renderer =
+  let component renderer to_js from_js =
     let rfunc this _ =
       let props = Js.Unsafe.get this "props" in
       let value = Js.Unsafe.get props "value" in
+      let value' = from_js value in
       (* let statics = Js.Unsafe.get props "statics" in *)
-      renderer value
+      renderer value'
     in
     let opts = to_obj [("render", inj @@ Js.wrap_meth_callback rfunc)] in
     let comp = Js.Unsafe.meth_call react "createClass" [| opts |] in
     let r value =
-      let opts = to_obj [("value", inj value)] in
+      let value' = to_js value in
+      let opts = to_obj [("value", inj value')] in
       Js.Unsafe.meth_call react "createElement" [| comp; opts |]
     in r
 
@@ -52,8 +54,6 @@ module React:REACT = struct
                            inj @@ to_obj opts;
                            inj @@ jss children |]
 
-
-
   let render comp node =
     Js.Unsafe.meth_call react "render" [| inj comp; inj node |]
 end
@@ -62,11 +62,13 @@ let box = React.component
             (fun v ->
              React.element_of_tag "div" [("className",
                                           inj @@ jss "commentBox")]
-                                  "I am comment box")
+                                  v)
+            (fun ov -> jss ov)
+            (fun jsv -> Js.to_string jsv)
 
 let start _:(bool Js.t) =
   let div = Dom_html.getElementById "main-area" in
-  let () = React.render (box ()) div in
+  let () = React.render (box "I am comment box") div in
   Js._false
 
 let () =
