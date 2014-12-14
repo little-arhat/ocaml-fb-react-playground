f4e5dd5ca2bf646bf6bdc89248104911f9445827
Roma Sokolov <sokolov.r.v@gmail.com>
2014-12-14 21:19:34 +0300

    [change] first class modules

--- src/core.ml
+++ src/core.ml
@@ -6,14 +6,21 @@ let to_obj l = Js.Unsafe.obj @@ Array.of_list l
 let jss s = Js.string s
 let inj o = Js.Unsafe.inject o
 
-module type REACT = sig
-    type component
-    val create_class : ('a -> 'b -> 'c ) -> component
-    val element_of_component : component -> (string * Js.Unsafe.any) list -> component
-    val element_of_tag : string -> (string * Js.Unsafe.any) list -> string -> component
-    val render : component -> Dom_html.element Js.t -> unit
+module type COMPONENT = sig
+    type arg
+    type jsval
+    val to_js : arg -> jsval
+    val from_js : jsval -> arg
+    val render : arg -> 'a (* REACT.component *)
   end
 
+module type REACT = sig
+    type component
+    val element_of_tag : string -> (string * Js.Unsafe.any) list -> string -> 'a (* component *)
+    val render : component -> Dom_html.element Js.t -> unit
+
+    val component : (module COMPONENT with type arg = 'a) -> ('a -> component)
+  end
 
 module React:REACT = struct
   let react = (Js.Unsafe.variable "React")
@@ -25,7 +32,7 @@ module React:REACT = struct
     Js.Unsafe.meth_call react "createClass" [| comp_opts |]
 
   let element_of_component comp opts =
-    Js.Unsafe.meth_call react "createElement" [| comp; inj @@ to_obj opts |]
+    Js.Unsafe.meth_call react "createElement" [| comp; to_obj opts |]
 
   let element_of_tag tag opts children =
     Js.Unsafe.meth_call react "createElement"
@@ -33,39 +40,23 @@ module React:REACT = struct
                            inj @@ to_obj opts;
                            inj @@ jss children |]
 
+  let component (type a) (module Comp:COMPONENT with type arg = a) =
+    let render_callback this _ =
+      let props = Js.Unsafe.get this "props" in
+      let value = Js.Unsafe.get props "value" in
+      let value' = Comp.from_js value in
+      Comp.render value'
+    in
+    let comp = create_class render_callback in
+    let r value =
+      element_of_component comp [("value", inj @@ Comp.to_js value)]
+    in r
+
   let render comp node =
     Js.Unsafe.meth_call react "render" [| inj comp; inj node |]
 end
 
-module type COMPONENT = sig
-    type arg
-    type jsval
-    val to_js : arg -> jsval
-    val from_js : jsval -> arg
-    val render : arg -> React.component
-  end
-
-module MakeComponent (Comp:COMPONENT) : sig
-  val init : Comp.arg -> React.component
-end
-  = struct
-  let react = (Js.Unsafe.variable "React")
-
-  let render_callback this _ =
-    let props = Js.Unsafe.get this "props" in
-    let value = Js.Unsafe.get props "value" in
-    let value' = Comp.from_js value in
-    Comp.render value'
-
-  let init value =
-    let comp = React.create_class render_callback in
-    let value' = Comp.to_js value in
-    React.element_of_component comp [("value", inj value')]
-
-end
-
-
-module CommentBoxSpec = struct
+module CommentBox = struct
   type arg = string
   type jsval = Js.js_string Js.t
 
@@ -77,11 +68,11 @@ module CommentBoxSpec = struct
                                                inj @@ jss "commentBox")] st
 end
 
-module CommentBox = MakeComponent(CommentBoxSpec)
+let comment_box = React.component (module CommentBox)
 
 let start _:(bool Js.t) =
   let div = Dom_html.getElementById "main-area" in
-  let () = React.render (CommentBox.init "This is a comment box") div in
+  let () = React.render (comment_box "This is a comment box") div in
   Js._false
 
 let () =
